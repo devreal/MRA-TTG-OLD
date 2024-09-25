ca7449e02e915164321e865dbdfdc22446666da2
Joseph Schuchart <joseph.schuchart@stonybrook.edu>
2024-09-25 09:49:26 +0800

    Pull in fixes from upstream
    - Several fixes to tensor views
    - Fixes for non-device compilation
    - Fixes for loop indexes in kernels
    
    Signed-off-by: Joseph Schuchart <joseph.schuchart@stonybrook.edu>

--- mra/madness/mra-device/kernels.h
+++ mra/madness/mra-device/kernels.h
@@ -9,23 +9,16 @@
 #include "types.h"
 #include "ttg/device/device.h"
 
-#if defined(TTG_ENABLE_CUDA)
-#include <cuda.h>
-typedef cudaStream_t stream_t;
-#elif defined(TTG_ENABLE_HOST)
-typedef decltype(ttg::device::current_stream()) stream_t;
-#else
-#warning Unknown device model, please add appropriate header includes!
-#endif
-
 /* Returns the total size of temporary memory needed for
  * the project() kernel. */
 template<mra::Dimension NDIM>
 std::size_t project_tmp_size(std::size_t K) {
   const size_t K2NDIM = std::pow(K,NDIM);
-  return (NDIM*K2NDIM) // xvec in fcube
+  const std::size_t TWOK2NDIM = std::pow(2*K, NDIM);
+  return (3*TWOK2NDIM) // workspace, values and r
+       + (NDIM*K2NDIM) // xvec in fcube
        + (NDIM*K)      // x in fcube
-       + (4*K2NDIM);   // workspace in transform, values, child_values, r
+       + (3*K2NDIM);   // workspace in transform, child_values, r
 }
 
 /* Explicitly instantiated for 1, 2, 3 dimensional Gaussians */
@@ -41,15 +34,14 @@ void submit_fcoeffs_kernel(
   T* tmp,
   bool* is_leaf_scratch,
   T thresh,
-  stream_t stream);
+  ttg::device::Stream stream);
 
 template<mra::Dimension NDIM>
 std::size_t compress_tmp_size(std::size_t K) {
   const size_t TWOK2NDIM = std::pow(2*K,NDIM);
   const size_t K2NDIM = std::pow(K,NDIM);
-  return (TWOK2NDIM) // s
-          + K2NDIM // workspace
-          + mra::Key<NDIM>::num_children // sumsq for each child and result
+  return (2*TWOK2NDIM) // s & workspace
+          + mra::Key<NDIM>::num_children() // sumsq for each child and result
           ;
 }
 
@@ -63,14 +55,13 @@ void submit_compress_kernel(
   const mra::TensorView<T, 2>& hgT_view,
   T* tmp,
   T* sumsqs,
-  const std::array<const T*, mra::Key<NDIM>::num_children>& in_ptrs,
-  stream_t stream);
+  const std::array<const T*, mra::Key<NDIM>::num_children()>& in_ptrs,
+  ttg::device::Stream stream);
 
 template<mra::Dimension NDIM>
 std::size_t reconstruct_tmp_size(std::size_t K) {
-  const size_t TWOK2NDIM = std::pow(2*K,NDIM); // s
-  const size_t K2NDIM = std::pow(K,NDIM); // workspace
-  return TWOK2NDIM + K2NDIM;
+  const size_t TWOK2NDIM = std::pow(2*K,NDIM); // s & workspace
+  return 2*TWOK2NDIM;
 }
 
 
@@ -80,8 +71,9 @@ void submit_reconstruct_kernel(
   mra::TensorView<T, NDIM>& node,
   const mra::TensorView<T, 2>& hg,
   const mra::TensorView<T, NDIM>& from_parent,
-  const std::array<T*, mra::Key<NDIM>::num_children>& r_arr,
+  const std::array<T*, mra::Key<NDIM>::num_children()>& r_arr,
   T* tmp,
-  stream_t stream);
+  std::size_t K,
+  ttg::device::Stream stream);
 
 #endif // HAVE_KERNELS_H
