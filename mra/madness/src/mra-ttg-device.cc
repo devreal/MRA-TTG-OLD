7f8905424c99a30fc9ff8d21eac99ca3ee792281
nilesh <n.chaturvedi3@gmail.com>
2024-11-25 22:28:03 +0530

    norm test draft

--- mra/madness/src/mra-ttg-device.cc
+++ mra/madness/src/mra-ttg-device.cc
@@ -535,7 +535,7 @@ auto make_gaxpy(ttg::Edge<mra::Key<NDIM>, mra::FunctionsCompressedNode<T, NDIM>>
 
     if (t1.empty() && t2.empty()) {
       /* send out an empty result */
-      auto out = mra::FunctionsCompressedNode<T, NDIM>(key, N);
+      auto out = mra::FunctionsCompressedNode<T, NDIM>(key, N); // out -> result
       send_out(std::move(out));
     } else {
 
@@ -676,6 +676,12 @@ auto make_multiply(ttg::Edge<mra::Key<NDIM>, mra::FunctionsReconstructedNode<T, 
                              {"out", "S1", "S2"});
 }
 
+template <typename T, Dimension NDIM>
+auto make_norm(ttg::Edge<mra::Key<NDIM>, mra::FunctionsCompressedNode<T, NDIM>> input)
+
+// computes from bottom up
+// task2: receive norm from children, compute on self, send send up
+
 /**
  * Test MRA projection with K coefficients in each of the NDIM dimension on
  * N random Gaussian functions.
@@ -715,6 +721,75 @@ void test(std::size_t N, std::size_t K) {
   auto compress = make_compress(N, K, functiondata, project_result, compress_result);
   auto reconstruct = make_reconstruct(N, K, functiondata, compress_result, reconstruct_result);
   auto gaxpy = make_gaxpy(compress_result, compress_result, gaxpy_result, T(1.0), T(-1.0), N, K);
+  auto multiply = make_multiply(reconstruct_result, reconstruct_result, multiply_result, functiondata, db, N, K);
+  auto printer =   make_printer(project_result,    "projected    ", false);
+  auto printer2 =  make_printer(compress_result,   "compressed   ", false);
+  auto printer3 =  make_printer(reconstruct_result,"reconstructed", false);
+  auto printer4 = make_printer(gaxpy_result, "gaxpy", false);
+  auto printer5 = make_printer(multiply_result, "multiply", false);
+
+  auto connected = make_graph_executable(start.get());
+  assert(connected);
+
+  std::chrono::time_point<std::chrono::high_resolution_clock> beg, end;
+  if (ttg::default_execution_context().rank() == 0) {
+      //std::cout << "Is everything connected? " << connected << std::endl;
+      //std::cout << "==== begin dot ====\n";
+      //std::cout << Dot()(start.get()) << std::endl;
+      //std::cout << "====  end dot  ====\n";
+
+      beg = std::chrono::high_resolution_clock::now();
+      // This kicks off the entire computation
+      start->invoke(mra::Key<NDIM>(0, {0}));
+  }
+  ttg::execute();
+  ttg::fence();
+
+  if (ttg::default_execution_context().rank() == 0) {
+    end = std::chrono::high_resolution_clock::now();
+    std::cout << "TTG Execution Time (milliseconds) : "
+              << (std::chrono::duration_cast<std::chrono::microseconds>(end - beg).count()) / 1000
+              << std::endl;
+  }
+}
+
+template<typename T, mra::Dimension NDIM>
+void test_pcr(std::size_t N, std::size_t K) {
+  auto functiondata = mra::FunctionData<T,NDIM>(K);
+  mra::Domain<NDIM> D;
+  D.set_cube(-6.0,6.0);
+
+  srand48(5551212); // for reproducible results
+  for (int i = 0; i < 10000; ++i) drand48(); // warmup generator
+
+  ttg::Edge<mra::Key<NDIM>, void> project_control;
+  ttg::Edge<mra::Key<NDIM>, mra::FunctionsReconstructedNode<T, NDIM>> project_result, reconstruct_result, multiply_result;
+  ttg::Edge<mra::Key<NDIM>, mra::FunctionsCompressedNode<T, NDIM>> compress_result, compress_reconstruct_result, gaxpy_result;
+
+  // define N Gaussians
+  std::vector<mra::Gaussian<T, NDIM>> gaussians;
+  gaussians.reserve(N);
+  // T expnt = 1000.0;
+  for (int i = 0; i < N; ++i) {
+    T expnt = 1500 + 1500*drand48();
+    mra::Coordinate<T,NDIM> r;
+    for (size_t d=0; d<NDIM; d++) {
+      r[d] = T(-6.0) + T(12.0)*drand48();
+    }
+    std::cout << "Gaussian " << i << " expnt " << expnt << std::endl;
+    gaussians.emplace_back(D, expnt, r);
+  }
+
+  // put it into a buffer
+  auto gauss_buffer = ttg::Buffer<mra::Gaussian<T, NDIM>>(gaussians.data(), N);
+  auto db = ttg::Buffer<mra::Domain<NDIM>>(&D);
+  auto start = make_start(project_control);
+  auto project = make_project(db, gauss_buffer, N, K, functiondata, T(1e-6), project_control, project_result);
+  auto compress = make_compress(N, K, functiondata, project_result, compress_result);
+  auto reconstruct = make_reconstruct(N, K, functiondata, compress_result, reconstruct_result);
+  auto compress_r = make_compress(N, K, functiondata, reconstruct_result, compress_reconstuct_result); // C(R(C(P)))
+
+  auto gaxpy = make_gaxpy(compress_result, compress_reconstruct_result, gaxpy_result, T(1.0), T(-1.0), N, K);
   auto multiply = make_multiply(reconstruct_result, reconstruct_result, multiply_result, functiondata, db, N, K);
   auto printer =   make_printer(project_result,    "projected    ", false);
   auto printer2 =  make_printer(compress_result,   "compressed   ", false);
