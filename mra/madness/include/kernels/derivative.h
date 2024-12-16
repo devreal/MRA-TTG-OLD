7f0fc28b35a25d873f7ab50caa02055f1054bb61
nilesh <n.chaturvedi3@gmail.com>
2024-12-16 11:00:46 +0530

    revert changes
    Put back computing the derivative operators in functiondata. Skeleton kernel applies the operator depending on whether the node is an internal or boundary.

--- mra/madness/include/kernels/derivative.h
+++ mra/madness/include/kernels/derivative.h
@@ -9,40 +9,46 @@
 namespace mra {
   namespace detail {
     template <typename T, Dimension NDIM>
+    DEVSCOPE void derivative_inner()
+    {
+      // diff2i()
+    }
+
+    template <typename T, Dimension NDIM>
+    DEVSCOPE void derivative_boundary(
+      const T* node_left,
+      const T* node_center,
+      const T* node_right,
+      bool axis) // axis to determine left or right boundary
+    {
+      //diff2b()
+    }
+
+    template <typename T, Dimension NDIM>
+    // need to implement diff2i() and diff2b()
     DEVSCOPE void derivative_kernel_impl(
-      T* r0,
-      T* rminus,
-      T* rplus,
-      size_type K,
-      mra::Key<NDIM> key)
-      {
-        const bool is_t0 = 0 == (threadIdx.x + threadIdx.y + threadIdx.z);
-        SHARED TensorView<T, 2> r_center, r_right, r_left;
-        if (is_t0) {
-          r_center = TensorView<T, 2>(r0, K);
-          r_right = TensorView<T, 2>(rplus, K);
-          r_left = TensorView<T, 2>(rminus, K);
-        }
-        SYNCTHREADS();
-
-        double iphase = 1.0;
-        /* wrap the loops into foreach_idx() and obtain gammaij and Kij somehow */
-        foreach_idx(r_center, [&](auto i, auto j) {
-          double jphase = 1.0;
-          double gammaij = std::sqrt(double((2*i+1)*(2*j+1)));
-          double Kij;
-          if (((i-j)>0) && (((i-j)%2)==1))
-            Kij = 2.0;
-          else
-            Kij = 0.0;
-
-          r_center(i,j) = T(0.5*(1.0 - iphase*jphase - 2.0*Kij)*gammaij);
-          r_left(i,j) = T(0.5*jphase*gammaij);
-          r_right(i,j) = T(-0.5*iphase*gammaij);
-        });
+      const T* node_left,
+      const T* node_center,
+      const T* node_right,
+      bool is_bdy
+    )
+      { // if we reached here, all checks have passed, and we do the transform to compute the derivative
+        // for a given axis by calling either derivative_inner() or derivative_boundary()
       }
 
+    template <typename T, Dimension NDIM>
+    GLOBALSCOPE void derivative_kernel()
+    {
+    }
+
   } // namespace detail
+
+  void submit_derivative_kernel()
+  {
+    // given a key(level, translation), check if any of the input nodes have any children
+    // if not, then we can compute the derivative by applying the
+  }
+
 } // namespace mra
 
 #endif // MRA_KERNELS_DERIVATIVE_H
