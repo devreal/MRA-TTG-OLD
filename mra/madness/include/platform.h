c5a03f575dc21141c2dc858cd724586d562ca5d0
Joseph Schuchart <joseph.schuchart@stonybrook.edu>
2025-01-03 12:24:24 -0500

    Fix platform type and macro selection
    Signed-off-by: Joseph Schuchart <joseph.schuchart@stonybrook.edu>

--- mra/madness/include/platform.h
+++ mra/madness/include/platform.h
@@ -3,6 +3,15 @@
 
 #include <cstdlib>
 
+#if defined(MRA_ENABLE_CUDA)
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+#elif defined(MRA_ENABLE_HIP)
+#include <hip/hip_runtime.h>
+#include <hip_runtime_api.h>
+#endif
+
 /**
  * Utilities to achieve platform independence.
  * Provides wrappers for CUDA/HIP constructs to compile code
@@ -21,6 +30,7 @@ namespace mra::detail {
 
 /* convenience macro to mark functions __device__ if compiling for CUDA */
 #if defined(__CUDA_ARCH__)
+#define GLOBALSCOPE __global__
 #define SCOPE __device__ __host__
 #define SYNCTHREADS() __syncthreads()
 #define DEVSCOPE __device__
@@ -28,6 +38,7 @@ namespace mra::detail {
 #define LAUNCH_BOUNDS(__NT) __launch_bounds__(__NT)
 #define HAVE_DEVICE_ARCH 1
 #elif defined(__HIPCC__)
+#define GLOBALSCOPE __global__
 #define SCOPE __device__ __host__
 #define SYNCTHREADS() __syncthreads()
 #define DEVSCOPE __device__
@@ -35,21 +46,13 @@ namespace mra::detail {
 #define LAUNCH_BOUNDS(__NT) __launch_bounds__(__NT)
 #define HAVE_DEVICE_ARCH 1
 #else // __CUDA_ARCH__
+#define GLOBALSCOPE
 #define SCOPE
 #define SYNCTHREADS() do {} while(0)
 #define DEVSCOPE inline
 #define SHARED
 #define LAUNCH_BOUNDS(__NT)
 #endif // __CUDA_ARCH__
-
-#ifdef HAVE_DEVICE_ARCH
-using Dim3 = dim3;
-#define GLOBALSCOPE __global__
-#else // HAVE_DEVICE_ARCH
-using Dim3 = mra::detail::dim3;
-#define GLOBALSCOPE
-#endif // HAVE_DEVICE_ARCH
-
 
 #if defined(__CUDACC__)
 #define checkSubmit() \
@@ -86,14 +89,19 @@ using Dim3 = mra::detail::dim3;
 #define THROW(s) do { throw std::runtime_error(s); } while(0)
 #endif // __CUDA_ARCH__
 
+#if defined(MRA_ENABLE_HOST)
+using Dim3 = mra::detail::dim3;
+#else
+using Dim3 = dim3;
+#endif // MRA_ENABLE_HOST
 
-#if defined(MRA_ENABLE_HOST) || (defined(MRA_ENABLE_HIP) && !defined(HAVE_DEVICE_ARCH))
+#if defined(MRA_ENABLE_HOST)
 namespace mra {
   /* define our own thread layout (single thread) */
-  static constexpr const detail::dim3 threadIdx = {0, 0, 0};
-  static constexpr const detail::dim3 blockDim  = {1, 1, 1};
-  static constexpr const detail::dim3 gridDim   = {1, 1, 1};
-  inline thread_local    detail::dim3 blockIdx  = {0, 0, 0};
+  static constexpr const mra::detail::dim3 threadIdx = {0, 0, 0};
+  static constexpr const mra::detail::dim3 blockDim  = {1, 1, 1};
+  static constexpr const mra::detail::dim3 gridDim   = {1, 1, 1};
+  inline thread_local    mra::detail::dim3 blockIdx  = {0, 0, 0};
 } // namespace mra
 #endif // MRA_ENABLE_HOST
 
@@ -102,11 +110,19 @@ namespace mra {
  */
 namespace mra {
   DEVSCOPE int thread_id() {
+#if defined(HAVE_DEVICE_ARCH)
     return blockDim.x * ((blockDim.y * threadIdx.z) + threadIdx.y) + threadIdx.x;
+#else  // HAVE_DEVICE_ARCH
+    return 0;
+#endif // HAVE_DEVICE_ARCH
   }
 
   DEVSCOPE int block_size() {
+#if defined(HAVE_DEVICE_ARCH)
     return blockDim.x * blockDim.y * blockDim.z;
+#else  // HAVE_DEVICE_ARCH
+    return 1;
+#endif // HAVE_DEVICE_ARCH
   }
 } // namespace mra
 
