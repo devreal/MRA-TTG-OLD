b3b7534e00308cabb9e06bc6cf3c2a50de0c1a84
Joseph Schuchart <joseph.schuchart@stonybrook.edu>
2025-01-08 14:11:19 -0500

    Draft implementation for tensor cycledim
    Signed-off-by: Joseph Schuchart <joseph.schuchart@stonybrook.edu>

--- mra/madness/include/cycledim.h
+++ mra/madness/include/cycledim.h
@@ -3,10 +3,43 @@
 
 #include "platform.h"
 #include "types.h"
+#include "tensorview.h"
 
 namespace mra{
   namespace detail{
-    SCOPE void cycledim(){}
+    template<typename T, Dimension NDIM>
+    SCOPE void cycledim(const TensorView<T, NDIM>& in, TensorView<T, NDIM>& out, int nshift, int start, int end){
+      std::array<int, NDIM> shifts;
+      // support python-style negative indexing
+      if (end < 0) {
+        end = NDIM - end;
+      }
+      if (start < 0) {
+        start = NDIM - start;
+      }
+      // compute new index positions
+      for (int i = 0; i < NDIM; ++i) {
+        if (i >= start && i < end) {
+          shifts[i] = (i - start + nshift) % (end - start) + start;
+        } else {
+          shifts[i] = i;
+        }
+      }
+      // assign using new index positions
+      foreach_idxs(in, [&](size_type... idxs){
+        std::array<int, NDIM> newidxs;
+        std::array<int, NDIM> idxs_arr = {idxs...};
+        /* mutate the indices */
+        for (int i = 0; i < NDIM; ++i) {
+          newidxs[shifts[i]] = idxs_arr[i];
+        }
+        T val = in(idxs...);
+        auto do_assign = [&]template<std::size_t... Is>(std::index_sequence_t){
+          out(newidxs[Is]...) = val;
+        };
+        do_assign(std::make_index_sequence<NDIM>{});
+      });
+    }
   }
 }
 
