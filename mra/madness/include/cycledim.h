ec6543706e4533f13248e997e440721f9b384664
Joseph Schuchart <joseph.schuchart@stonybrook.edu>
2025-01-13 17:05:41 -0500

    Fixes for cycledim
    Signed-off-by: Joseph Schuchart <joseph.schuchart@stonybrook.edu>

--- mra/madness/include/cycledim.h
+++ mra/madness/include/cycledim.h
@@ -26,7 +26,7 @@ namespace mra{
         }
       }
       // assign using new index positions
-      foreach_idxs(in, [&](size_type... idxs){
+      foreach_idxs(in, [&](auto... idxs){
         std::array<int, NDIM> newidxs;
         std::array<int, NDIM> idxs_arr = {idxs...};
         /* mutate the indices */
@@ -34,7 +34,7 @@ namespace mra{
           newidxs[shifts[i]] = idxs_arr[i];
         }
         T val = in(idxs...);
-        auto do_assign = [&]template<std::size_t... Is>(std::index_sequence_t){
+        auto do_assign = [&]template<std::size_t... Is>(std::index_sequence<Is...>){
           out(newidxs[Is]...) = val;
         };
         do_assign(std::make_index_sequence<NDIM>{});
