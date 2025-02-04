257f967f0b015d2fbb136faf1a82cade0d11663e
nilesh <n.chaturvedi3@gmail.com>
2025-02-04 11:42:34 -0500

    Rebase changes

--- mra/madness/include/kernels/reconstruct.h
+++ mra/madness/include/kernels/reconstruct.h
@@ -34,7 +34,7 @@ namespace mra {
       TensorView<T, NDIM>& s,
       TensorView<T, NDIM>& tmp_node,
       T* workspace,
-      std::array<TensorView<T, NDIM>, Key<NDIM>::num_children()>& r_arr)
+      std::array<T*, Key<NDIM>::num_children()>& r_arr)
     {
       s = 0.0;
       tmp_node = node;
@@ -49,7 +49,7 @@ namespace mra {
       for (size_type i = 0; i < key.num_children(); ++i) {
         auto child_slice = get_child_slice<NDIM>(key, K, i);
         /* tmp layout: 2K^NDIM for s, K^NDIM for workspace, [K^NDIM]* for r fields */
-        auto& r = r_arr[i];
+        auto r = TensorView<T, NDIM>(r_arr[i], K);
         r = s(child_slice);
       }
     }
@@ -57,7 +57,7 @@ namespace mra {
 
     template<typename T, Dimension NDIM>
     GLOBALSCOPE void
-    LAUNCH_BOUNDS(max_threads(2*MRA_MAX_K))
+    LAUNCH_BOUNDS(MRA_MAX_K*MRA_MAX_K)
     reconstruct_kernel(
       Key<NDIM> key,
       size_type N,
@@ -66,12 +66,14 @@ namespace mra {
       T* tmp_ptr,
       const TensorView<T, 2> hg,
       const TensorView<T, NDIM+1> from_parent_view,
-      std::array<TensorView<T, NDIM+1>, Key<NDIM>::num_children()> r_arr)
+      std::array<T*, Key<NDIM>::num_children()> r_arr)
     {
       const bool is_t0 = (0 == thread_id());
+      const size_type TWOK2NDIM = std::pow(2*K,NDIM);
+      const size_type K2NDIM    = std::pow(  K,NDIM);
 
       /* pick the r's for this function */
-      SHARED std::array<TensorView<T, NDIM>, Key<NDIM>::num_children()> block_r_arr;
+      SHARED std::array<T*, Key<NDIM>::num_children()> block_r_arr;
       SHARED TensorView<T, NDIM> s, tmp_node;
       SHARED T* workspace;
       SHARED TensorView<T, NDIM> node, from_parent;
@@ -79,7 +81,6 @@ namespace mra {
       size_type blockId = blockIdx.x;
       T* block_tmp_ptr = &tmp_ptr[blockId*reconstruct_tmp_size<NDIM>(K)];
       if (is_t0) {
-        const size_type TWOK2NDIM = std::pow(2*K,NDIM);
         s           = TensorView<T, NDIM>(&block_tmp_ptr[0], 2*K);
         tmp_node    = TensorView<T, NDIM>(&block_tmp_ptr[1*TWOK2NDIM], 2*K);
         workspace   = &block_tmp_ptr[2*TWOK2NDIM];
@@ -90,7 +91,7 @@ namespace mra {
           node = node_view(fnid);
           from_parent = from_parent_view(fnid);
           for (size_type i = 0; i < Key<NDIM>::num_children(); ++i) {
-            block_r_arr[i] = r_arr[i](fnid);
+            block_r_arr[i] = &r_arr[i][K2NDIM*fnid];
           }
         }
         SYNCTHREADS();
@@ -107,11 +108,12 @@ namespace mra {
     TensorView<T, NDIM+1>& node,
     const TensorView<T, 2>& hg,
     const TensorView<T, NDIM+1>& from_parent,
-    const std::array<TensorView<T, NDIM+1>, mra::Key<NDIM>::num_children()>& r_arr,
+    const std::array<T*, mra::Key<NDIM>::num_children()>& r_arr,
     T* tmp,
     ttg::device::Stream stream)
   {
-    Dim3 thread_dims = max_thread_dims(2*K);
+    size_type max_threads = std::min(K, MRA_MAX_K_SIZET);
+    Dim3 thread_dims = Dim3(max_threads, max_threads, 1);
     CALL_KERNEL(detail::reconstruct_kernel, N, thread_dims, 0, stream,
       (key, N, K, node, tmp, hg, from_parent, r_arr));
     checkSubmit();
