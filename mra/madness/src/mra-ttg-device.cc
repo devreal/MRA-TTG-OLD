f2614f5bc620b89c29b80a9e54fcf58b1c266ad4
nilesh <n.chaturvedi3@gmail.com>
2025-02-11 15:48:06 -0500

    Test case for derivaitve task

--- mra/madness/src/mra-ttg-device.cc
+++ mra/madness/src/mra-ttg-device.cc
@@ -9,6 +9,7 @@
 #include "key.h"
 #include "domain.h"
 #include "options.h"
+#include <any>
 
 #include <ttg/serialization/backends.h>
 #include <ttg/serialization/std/array.h>
@@ -663,6 +664,31 @@ auto make_gaxpy(ttg::Edge<mra::Key<NDIM>, mra::FunctionsCompressedNode<T, NDIM>>
 }
 
 template<typename T, mra::Dimension NDIM>
+auto make_gaxpy(ttg::Edge<mra::Key<NDIM>, mra::FunctionsReconstructedNode<T, NDIM>> in1,
+              ttg::Edge<mra::Key<NDIM>, mra::FunctionsReconstructedNode<T, NDIM>> out,
+              const T scalarA,
+              const size_t N,
+              const size_t K,
+              const char* name = "gaxpy_ovld")
+{
+  auto func = [N](const mra::Key<NDIM>& key){
+    return FunctionsCompressedNode<T, NDIM>(key, N);
+  };
+
+  auto rank = ttg::default_execution_context().rank();
+
+  auto pmap = [rank](const mra::Key<NDIM>& key){
+    return rank;
+  };
+
+  ttg::Edge<mra::Key<NDIM>, mra::FunctionsCompressedNode<T, NDIM>> dummy{"gaxpy_dummy", true,
+                                                                          {std::any{}, std::move(func), std::move(pmap)}};
+
+
+  return make_gaxpy(in1, dummy, out, scalarA, 0.0, N, K, name);
+}
+
+template<typename T, mra::Dimension NDIM>
 auto make_multiply(ttg::Edge<mra::Key<NDIM>, mra::FunctionsReconstructedNode<T, NDIM>> in1,
               ttg::Edge<mra::Key<NDIM>, mra::FunctionsReconstructedNode<T, NDIM>> in2,
               ttg::Edge<mra::Key<NDIM>, mra::FunctionsReconstructedNode<T, NDIM>> out,
@@ -1218,6 +1244,92 @@ void test_pcr(std::size_t N, std::size_t K) {
 
   // C(R(C(P))) - C(P)
   auto gaxpy = make_gaxpy(compress_reconstruct_result, compress_result, gaxpy_result, T(1.0), T(-1.0), N, K);
+  // | C(R(C(P))) - C(P) |
+  auto norm  = make_norm(N, K, gaxpy_result, norm_result);
+  // final check
+  auto norm_check = ttg::make_tt([&](const mra::Key<NDIM>& key, const mra::Tensor<T, 1>& norms){
+    // TODO: check for the norm within machine precision
+    auto norms_arr = norms.buffer().current_device_ptr();
+    for (size_type i = 0; i < N; ++i) {
+      std::cout << "Final norm " << i << ": " << norms_arr[i] << std::endl;
+    }
+  }, ttg::edges(norm_result), ttg::edges(), "norm-check");
+
+  auto connected = make_graph_executable(start.get());
+  assert(connected);
+
+  std::chrono::time_point<std::chrono::high_resolution_clock> beg, end;
+  if (ttg::default_execution_context().rank() == 0) {
+      //std::cout << "Is everything connected? " << connected << std::endl;
+      //std::cout << "==== begin dot ====\n";
+      //std::cout << Dot()(start.get()) << std::endl;
+      //std::cout << "====  end dot  ====\n";
+
+      beg = std::chrono::high_resolution_clock::now();
+      // This kicks off the entire computation
+      start->invoke(mra::Key<NDIM>(0, {0}));
+  }
+  ttg::execute();
+  ttg::fence();
+
+  if (ttg::default_execution_context().rank() == 0) {
+    end = std::chrono::high_resolution_clock::now();
+    std::cout << "TTG Execution Time (milliseconds) : "
+              << (std::chrono::duration_cast<std::chrono::microseconds>(end - beg).count()) / 1000
+              << std::endl;
+  }
+}
+
+template<typename T, mra::Dimension NDIM>
+void test_derivative(std::size_t N, std::size_t K) {
+  auto functiondata = mra::FunctionData<T,NDIM>(K);
+  auto D = std::make_unique<mra::Domain<NDIM>[]>(1);
+  D[0].set_cube(-6.0,6.0);
+  T g1 = 0;
+  T g2 = 0;
+  Dimension axis = 3;
+
+  srand48(5551212); // for reproducible results
+  for (int i = 0; i < 10000; ++i) drand48(); // warmup generator
+
+  ttg::Edge<mra::Key<NDIM>, void> project_control;
+  ttg::Edge<mra::Key<NDIM>, mra::FunctionsReconstructedNode<T, NDIM>> project_result, reconstruct_result, multiply_result;
+  ttg::Edge<mra::Key<NDIM>, mra::FunctionsCompressedNode<T, NDIM>> compress_result, compress_derivative_result, gaxpy_result;
+  ttg::Edge<mra::Key<NDIM>, mra::FunctionsReconstructedNode<T, NDIM>> derivative_result;
+  ttg::Edge<mra::Key<NDIM>, mra::Tensor<T, 1>> norm_result;
+
+  // define N Gaussians
+  auto gaussians = std::make_unique<mra::Gaussian<T, NDIM>[]>(N);
+  T expnt = 1000.0;
+  T factor = expnt;
+
+  for (int i = 0; i < N; ++i) {
+    // T expnt = 1500 + 1500*drand48();
+    mra::Coordinate<T,NDIM> r;
+    for (size_t d=0; d<NDIM; d++) {
+      r[d] = T(-6.0) + T(12.0)*drand48();
+    }
+    std::cout << "Gaussian " << i << " expnt " << expnt << std::endl;
+    gaussians[i] = mra::Gaussian<T, NDIM>(D[0], expnt, r);
+  }
+
+  // put it into a buffer
+  auto gauss_buffer = ttg::Buffer<mra::Gaussian<T, NDIM>>(std::move(gaussians), N);
+  auto db = ttg::Buffer<mra::Domain<NDIM>>(std::move(D), 1);
+  auto start = make_start(project_control);
+  auto project = make_project(db, gauss_buffer, N, K, functiondata, T(1e-6), project_control, project_result);
+  // C(P)
+  auto compress = make_compress(N, K, functiondata, project_result, compress_result, "compress-cp");
+  // // R(C(P))
+  auto reconstruct = make_reconstruct(N, K, functiondata, compress_result, reconstruct_result, "reconstruct-rcp");
+  // C(R(C(P)))
+  auto derivative = make_derivative(N, K, reconstruct_result, derivative_result, functiondata, db, g1, g2, axis,
+                                    FunctionData<T, NDIM>::BC_DIRICHLET, FunctionData<T, NDIM>::BC_DIRICHLET, "derivative");
+
+  auto compress_r = make_compress(N, K, functiondata, derivative_result, compress_derivative_result, "compress-crcp");
+
+  // C(R(C(P))) - C(P)
+  auto gaxpy = make_gaxpy(compress_derivative_result, gaxpy_result, factor, N, K, "gaxpy_deriv");
   // | C(R(C(P))) - C(P) |
   auto norm  = make_norm(N, K, gaxpy_result, norm_result);
   // final check
